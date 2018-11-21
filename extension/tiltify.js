6dce9a53974d7079d1d9ed9a5d4c3f75097e9c7f
oliver <o-lohr@gmx.de>
2018-11-21 16:03:11 +0100

    Improved tiltify fake donations/polls/challenges

--- extension/tiltify.js
+++ extension/tiltify.js
@@ -24,19 +24,93 @@ const tiltifyCluster = "mt1";
 
 if (nodecg.bundleConfig && nodecg.bundleConfig.tiltify && nodecg.bundleConfig.tiltify.enable) {
 	if (enableTiltifyTestDonations) {
+
+		// testing, create fake donations and polls
+		const testWords = ['Lorem', "Ipsum", "Dolar","Si","Achmet","Greetings","From","Germany"];
+		function randSentence(minWords, maxWords) {
+			var words = minWords + Math.floor((maxWords-minWords)*Math.random());
+			var sentence = "";
+			for (var i = 1;i<words;i++) {
+				sentence += testWords[Math.floor(Math.random()*testWords.length)] + " ";
+			}
+			sentence += testWords[Math.floor(Math.random()*testWords.length)];
+			return sentence;
+		}
 		nodecg.log.info('Tiltify enable in test mode, only fake donations');
+		var pollId = 0;
+		var pollOptionId = 0;
+		var challengeId = 0;
 		var testDonations = [];
 		var testPolls = [];
 		var testChallenges = [];
 		var testCampaign = {"amountRaised":0};
+
+		// create test challenges
+		for(var i = 0;i<8;i++) {
+			testChallenges.push({
+				"id":challengeId,
+				"type":"Challenge",
+				"name":randSentence(5,7),
+				"totalAmountRaised":0,
+				"amount":500+Math.floor(Math.random()*500),
+				"campaignId":12345,
+				"active":true,
+				"endsAt":Date.now()+3600000,// ends in an hour
+				"createdAt":Date.now(),
+				"updatedAt":Date.now()});
+			challengeId++;
+		}
+		// create test polls
+		for(var i = 0;i<8;i++) {
+			var nextPoll = {
+				"id":pollId,
+				"name":randSentence(4,8),
+				"active":true,
+				"type":"Poll",
+				"campaignId":12345,
+				"createdAt":Date.now(),
+				"updatedAt":Date.now(),
+				"options":[]};
+			// testoptions
+			for(var j=0;j<3;j++) {
+				nextPoll.options.push({
+					"id":pollOptionId,
+					"pollId":pollId,
+					"name":randSentence(1,3),
+					"type":"PollOption",
+					"totalAmountRaised":0,
+					"createdAt":Date.now(),
+					"updatedAt":Date.now()});
+				pollOptionId++;
+			}
+			testPolls.push(nextPoll);
+			pollId++;
+		}
 		var did = 0;
 		function sendFakeDonation() {
 			if (testDonations.length >= 10) {
 				testDonations.pop();
 			}
 			var donationAmount = Math.floor(Math.random()*200 + 1);
-			var testDono = {"id":did,"amount":donationAmount,"name":"donatorname","comment":"Greetings from germany!",
-			"completedAt":1541438000000,"pollOptionId":123,"sustained":false};
+			var testDono = {"id":did,"amount":donationAmount,"name":randSentence(1,2),"comment":randSentence(3,7),
+				"completedAt":Date.now(),"sustained":false};
+			
+			// this donation either goes to nothing, a challenge or a poll
+			var benefitOpt = Math.random();
+			if (benefitOpt<0.3) {
+				// supports nothing
+			} else if (benefitOpt<0.6) {
+				// supports a challenge
+				var selectedChallenge = testChallenges[Math.floor(Math.random()*testChallenges.length)];
+				selectedChallenge.totalAmountRaised += testDono.amount;
+				testDono.challengeId = selectedChallenge.challengeId;
+			} else {
+				// supports a poll, grab poll fist...
+				var selectedOptions = testPolls[Math.floor(Math.random()*testPolls.length)].options;
+				var selectedOption = selectedOptions[Math.floor(Math.random()*selectedOptions.length)];
+				selectedOption.totalAmountRaised += testDono.amount;
+				testDono.pollOptionId = selectedOption.pollOptionId;
+			}
 			testDonations.unshift(testDono);
 			testCampaign.amountRaised += donationAmount;
 			_processRawDonation(testDono);
@@ -84,7 +158,14 @@ function setUpPusher() {
 	channel.bind('campaign', _processRawCampain);
 }
 
+/**
+ * Datastructure: {"type":"donation","data":{"id":1234,"amount":1,"name":"donatorname","comment":"Greetings from germany!",
+ * "completedAt":1541438000000,"pollOptionId":123,"sustained":false}}
+ * 
+ * Either pollOptionId, rewardId, challengeId or none of them are present, linking to the specified resource
+ */
 function _processRawDonation(donation) {
+	nodecg.log.info("donation: "+JSON.stringify(donation));
 	nodecg.sendMessage('newDonation', donation);
 	doUpdate();
 }
@@ -111,6 +192,7 @@ function reqCampain() {
 }
 
 function _processRawCampain(data) {
+	nodecg.log.info("campaign: "+JSON.stringify(data));
 	// Update the donation total replicant if it has actually changed.
 	if (data.amountRaised && donationTotal.value != data.amountRaised)
 		donationTotal.value = data.amountRaised;
@@ -131,6 +213,7 @@ function reqDonations() {
 }
 
 function _processRawDonations(data) {
+	nodecg.log.info("donations: "+JSON.stringify(data));
 	if (donations.value != data) {
 		donations.value = data;
 	}
@@ -150,9 +233,13 @@ function reqPolls() {
 }
 
 /**
- * Datastructure: TODO
+ * Datastructure: [{"id":123,"name":"Polldescription","active":false,"type":"Poll","campaignId":12345,"createdAt":1525099745000,
+ * "updatedAt":1526663265000,"options":
+ * [{"id":1234,"pollId":123,"name":"Option 1","type":"PollOption",
+ * "totalAmountRaised":8,"createdAt":1525099745000,"updatedAt":1525099745000}]}]
  */
 function _processRawPolls(data) {
+	nodecg.log.info("polls: "+JSON.stringify(data));
         if (polls.value != data)
                 polls.value = data;
 }
@@ -171,10 +258,12 @@ function reqChallenges() {
 }
 
 /**
- * Datastructure: {"type":"donation","data":{"id":1234,"amount":1,"name":"donatorname","comment":"Greetings from germany!",
- * "completedAt":1541438000000,"pollOptionId":123,"sustained":false}}
+ * 
+ * [{"id":1234,"type":"Challenge","name":"PollName","totalAmountRaised":0,"amount":500,
+ * "campaignId":12345,"active":false,"endsAt":1524369600000,"createdAt":1523551888000,"updatedAt":1523553738000}]
  */
 function _processRawChallenges(data) {
+	nodecg.log.info("challenges: "+JSON.stringify(data));
         if (incentives.value != data)
 		incentives.value = data;
 }
